f7cef7a84b31d9fb23286fbe260abecc941afda1
Stein-N <nico-stein-98@outlook.de>
2023-10-20 14:28:04 +0200

    Initial commit

---
+++ src/main/java/me/xstopho/resourcecompression/ResourceCompressionClient.java
@@ -0,0 +1,10 @@
+package me.xstopho.resourcecompression;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ResourceCompressionClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+
+	}
+}
---
+++ src/main/java/me/xstopho/resourcecompression/ResourceCompressionDataGenerator.java
@@ -0,0 +1,11 @@
+package me.xstopho.resourcecompression;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ResourceCompressionDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
